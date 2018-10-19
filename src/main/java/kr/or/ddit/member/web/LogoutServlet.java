4ff639b2174120881f4d56319b34b39a76cb047f
KuInSoo <kkuinsoo@gmail.com>
2018-10-19 18:30:59 +0900

    로그아웃기능

---
+++ src/main/java/kr/or/ddit/member/web/LogoutServlet.java
@@ -0,0 +1,31 @@
+package kr.or.ddit.member.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * ${PACKAGE_NAME}
+ * ${FILE_NAME}
+ * Desc :
+ *
+ * @Author : Mr.KKu
+ * @Date : 2018-10-19 / 오후 3:33
+ * @Version :
+ */
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.invalidate();
+		response.sendRedirect("/");
+	}
+}
